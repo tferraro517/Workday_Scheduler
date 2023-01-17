861260359f402d60a3b2306277d29da73bbd9827
Taryn <118402182+tferraro517@users.noreply.github.com>
2023-01-16 19:47:17 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -44,11 +44,11 @@ $(function () {
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
   $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-  $('#hour-1 .description').val(localStorage.getItem('hour-1'));
-  $('#hour-2 .description').val(localStorage.getItem('hour-2'));
-  $('#hour-3 .description').val(localStorage.getItem('hour-3'));
-  $('#hour-4 .description').val(localStorage.getItem('hour-4'));
-  $('#hour-5 .description').val(localStorage.getItem('hour-5'));
+  $('#hour-1 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-2 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-3 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-4 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-5 .description').val(localStorage.getItem('hour-17'));
 
 
 });
