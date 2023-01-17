13b4c72031beceeaf58a49da1b55bccc57c80e83
Taryn <118402182+tferraro517@users.noreply.github.com>
2023-01-16 19:52:00 -0500

    Update script.js
    changing hours to military time

--- Assets/script.js
+++ Assets/script.js
@@ -44,11 +44,11 @@ $(function () {
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
   $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-  $('#hour-1 .description').val(localStorage.getItem('hour-13'));
-  $('#hour-2 .description').val(localStorage.getItem('hour-14'));
-  $('#hour-3 .description').val(localStorage.getItem('hour-15'));
-  $('#hour-4 .description').val(localStorage.getItem('hour-16'));
-  $('#hour-5 .description').val(localStorage.getItem('hour-17'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 
 });
